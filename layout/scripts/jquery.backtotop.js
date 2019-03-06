eccd8c6ceb43afe2bdc7a6ad4ca4560dd6283aa8
aminu <aminusanti@gmail.com>
2019-03-06 15:08:44 +0000

    yes pls

--- layout/scripts/jquery.backtotop.js
+++ layout/scripts/jquery.backtotop.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 Template Name: Carinary
 Author: <a href="http://www.os-templates.com/">OS Templates</a>
@@ -19,26 +18,4 @@ jQuery(window).scroll(function () {
     } else {
         jQuery("#backtotop").removeClass("visible");
     }
-=======
-/*
-Template Name: Carinary
-Author: <a href="http://www.os-templates.com/">OS Templates</a>
-Author URI: http://www.os-templates.com/
-Licence: Free to use under our free template licence terms
-Licence URI: http://www.os-templates.com/template-terms
-File: Back to Top JS
-*/
-
-jQuery("#backtotop").click(function () {
-    jQuery("body,html").animate({
-        scrollTop: 0
-    }, 600);
-});
-jQuery(window).scroll(function () {
-    if (jQuery(window).scrollTop() > 150) {
-        jQuery("#backtotop").addClass("visible");
-    } else {
-        jQuery("#backtotop").removeClass("visible");
-    }
->>>>>>> e8224d319ff4946f0d589dd31d71c97999e9b423
 });
